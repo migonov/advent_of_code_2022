e48fbbeddb85e0839a67571c958f8d4c53c69d20
migonov <migonpl2000@gmail.com>
2023-07-29 19:12:56 +0200

    day5 solution

---
+++ day5/day5.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+var stacks [][]string
+
+func main() {
+	scanner := bufio.NewScanner(os.Stdin)
+
+	var inputLines []string
+	for scanner.Scan() {
+		input := scanner.Text()
+		if input == "" {
+			break
+		}
+		inputLines = append(inputLines, input)
+	}
+	stacks = parseStacks(inputLines)
+
+	for scanner.Scan() {
+		input := scanner.Text()
+		if input == "" {
+			break
+		}
+
+		splittedInput := strings.Split(input, " ")
+		n, _ := strconv.Atoi(splittedInput[1])
+		fromStack, _ := strconv.Atoi(splittedInput[3])
+		toStack, _ := strconv.Atoi(splittedInput[5])
+		moveCrates(n, fromStack-1, toStack-1)
+	}
+
+	for _, stack := range stacks {
+		fmt.Print(stack[len(stack)-1])
+	}
+}
+
+func moveCrates(n int, from int, to int) {
+	for i := 0; i < n; i++ {
+		stacks[to] = append(stacks[to], stacks[from][len(stacks[from])-1-i])
+	}
+	stacks[from] = stacks[from][:len(stacks[from])-n]
+}
+
+func parseStacks(inputLines []string) [][]string {
+	parsedInput := make([][]string, (len(inputLines[0])+1)/4)
+	for _, line := range inputLines[:len(inputLines)-1] {
+		stackNumber := 0
+		for index, character := range line {
+			if index%4 == 1 {
+				if character != ' ' {
+					parsedInput[stackNumber] = append([]string{string(character)}, parsedInput[stackNumber]...)
+				}
+				stackNumber++
+			}
+		}
+	}
+	return parsedInput
+}
